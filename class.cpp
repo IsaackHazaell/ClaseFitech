c8b08dfe91a6da934ab9091c42fda0c884421265
IsaackHazaell <isaachvc@hotmail.com>
2019-10-04 09:04:01 -0500

    Se añadio el metodo perimetro

--- class.cpp
+++ class.cpp
@@ -10,14 +10,26 @@ class Rectangulo()
        float largo, ancho;
     //Metodos
     public:
+        Rectangulo(float,float); //Constructor
         void perimetro();
         void area();
 }
 
+//Constructor
 Rectangulo::Rectangulo(float opc_largo, float opc_ancho)
 {
     largo = opc_largo;
     ancho = opc_ancho;
+}
+
+//Metodos
+void Rectangulo::perimetro()
+{
+    float opc_perimetro;
+
+    opc_perimetro = (2*largo) + (2*ancho);
+
+    cout << "El perimetro es: " << opc_perimetro << endl;
 }
 
 int main()

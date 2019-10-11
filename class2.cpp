cb5a1e19524632cec0cd1803c3848d2041fa635b
IsaackHazaell <isaachvc@hotmail.com>
2019-10-11 07:41:51 -0500

    Se añadio el constructor y el metodo read

--- class2.cpp
+++ class2.cpp
@@ -6,15 +6,26 @@ using namespace std;
 class Person
 {
     //Encapsulamiento
-    private:
+    private: //Atributos
         int age;
         string name;
-    public:
+    public: //Metodos
         Person(int, string);
         void read();
         void eat();
         void run();
 };
+
+Person::Person(int _age, string _name)
+{
+    age = _age;
+    name = _name;
+}
+
+void Person::read()
+{
+    cout << "Hola soy " << name << " y estoy leyendo un libro" << endl;
+}
 
 int main()
 {

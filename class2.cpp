7f4ac9a6f3027f142dfd36a96f38e7a9245afa26
IsaackHazaell <isaachvc@hotmail.com>
2019-10-11 07:32:14 -0500

    Se creo la clase person para el ejercicio 2

---
+++ class2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+class Person
+{
+    //Encapsulamiento
+    private:
+        int age;
+        string name;
+    public:
+        Person(int, string);
+        void read();
+        void eat();
+        void run();
+};
+
+int main()
+{
+
+}

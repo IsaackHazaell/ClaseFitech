305bd3b2d549efa69d9b22f2b0a5e03943db1cd8
IsaackHazaell <isaachvc@hotmail.com>
2019-10-11 08:04:18 -0500

    Se creo el objeto de la clase Person

--- class2.cpp
+++ class2.cpp
@@ -9,7 +9,7 @@ class Person
     private: //Atributos
         int age;
         string name;
-    public: //Metodos
+    public: //Metodos o funcion
         Person(int, string);
         void read();
         void eat();
@@ -27,7 +27,23 @@ void Person::read()
     cout << "Hola soy " << name << " y estoy leyendo un libro" << endl;
 }
 
+void Person::eat()
+{
+    cout << "Mi nombre es " << name << " tengo "<< age <<" años y me gusta comer sushi" <<endl;
+}
+
+void Person::run()
+{
+    cout << "Mi amigos me dicen " << name << " y corro un maraton de 10km" << endl;
+}
+
 int main()
 {
+    Person a = Person(22,"Juanito");
+    a.read();
+    a.eat();
+    a.run();
 
+    system ("pause");
+    return 0;
 }
